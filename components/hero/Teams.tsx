831fefa531ce08f2b332195251268b9c41bd57fc
Angelika Ceborska <angelika.ceborska@gmail.com>
2026-02-15 16:06:48 +0000

    Add new logos

--- components/hero/Teams.tsx
+++ components/hero/Teams.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import { UnumLogo } from "./logos/UnumLogo";
 import { BiobarLogo } from "./logos/BiobarLogo";
 
@@ -5,7 +6,29 @@ export function Teams() {
   return (
     <div className="flex flex-col items-center gap-4">
       <p className="text-sm text-muted">I worked with the teams at:</p>
-      <div className="flex items-center gap-6">
+      <div className="flex items-center gap-6 opacity-50">
+        <div className="flex items-center gap-2">
+          <Image
+            src="/hero/Kerlig.png"
+            alt="Kerlig"
+            width={24}
+            height={24}
+            className="rounded-md grayscale"
+          />
+          <span className="text-sm font-semibold tracking-tighter">Kerlig</span>
+        </div>
+        <div className="flex items-center gap-2">
+          <Image
+            src="/hero/Journal.png"
+            alt="Journal"
+            width={24}
+            height={24}
+            className="rounded-md grayscale"
+          />
+          <span className="text-sm font-semibold tracking-tighter">
+            Journal
+          </span>
+        </div>
         <UnumLogo />
         <BiobarLogo />
       </div>
