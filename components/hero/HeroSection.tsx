a9438b6607c86d46e4f4857fcc7c93a70e8d64b2
Angelika Ceborska <angelika.ceborska@gmail.com>
2026-04-26 19:38:03 +0200

    Refactor HeroSection layout for improved responsiveness

--- components/hero/HeroSection.tsx
+++ components/hero/HeroSection.tsx
@@ -5,8 +5,8 @@ import { Carousel } from "./Carousel";
 export function HeroSection() {
   return (
     <section className="flex flex-col gap-[100px] pt-[100px] pb-20 overflow-hidden">
-      <div className="grid grid-cols-8 gap-4 w-full">
-        <div className="col-start-2 col-span-5 flex flex-col">
+      <div className="px-6 md:px-0 md:grid md:grid-cols-8 md:gap-4 w-full">
+        <div className="md:col-start-2 md:col-span-5 flex flex-col">
           <Headline />
           <Subtitle />
         </div>
